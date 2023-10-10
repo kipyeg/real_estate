4816ad5b060015b63a245d9d52ce9f76356c5662
kipyeg <MY_NAME@example.com>
2023-10-10 11:21:13 +0300

    create and run server

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom/dist";
-import "./App.css";
+import "./index.css";
 import Header from "./components/Header";
 import About from "./pages/About";
 import Home from "./pages/Home";
